b1708af8562bb20052d3936eb3dae302fc82591b
s1b030z <sushant.bhat@walmart.com>
2025-04-06 13:34:59 +0530

    Code smell removal

--- src/main/java/com/anthat/cineflix/service/TranscodeService.java
+++ src/main/java/com/anthat/cineflix/service/TranscodeService.java
@@ -1,7 +1,5 @@
 package com.anthat.cineflix.service;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 
 public interface TranscodeService {
--- src/main/java/com/anthat/cineflix/service/impl/FFMpegTranscodeService.java
+++ src/main/java/com/anthat/cineflix/service/impl/FFMpegTranscodeService.java
@@ -4,7 +4,6 @@ import com.anthat.cineflix.service.TranscodeService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;

6831cc523301b108d50c1fb5ccda0e2dd6f554f9
s1b030z <sushant.bhat@walmart.com>
2025-05-07 20:55:08 +0530

    Separate out services according to their potential traffic volume

---
+++ src/main/java/com/anthat/cineflix/service/VideoMetaService.java
@@ -0,0 +1,19 @@
+package com.anthat.cineflix.service;
+
+import com.anthat.cineflix.config.ModuleConfig;
+import com.anthat.cineflix.dto.VideoDTO;
+import com.anthat.cineflix.exception.VideoAccessException;
+import com.anthat.cineflix.exception.VideoUpdateException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface VideoMetaService {
+
+    VideoDTO getVideoInfoById(String videoId) throws VideoAccessException;
+
+    List<VideoDTO> getModuleVideos(ModuleConfig moduleConfig) throws VideoAccessException;
+
+    VideoDTO updateVideoInfo(VideoDTO videoDetails, String videoId) throws VideoUpdateException;
+}
